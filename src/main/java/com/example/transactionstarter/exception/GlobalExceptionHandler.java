c1f028bba668edfbda920c0d77c443c5812c072c
Venkat Aravind <venkataravind.123456@gmail.com>
2026-08-29 21:12:44 +0530

    Added Transaction not Found Exception

---
+++ src/main/java/com/example/transactionstarter/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.example.transactionstarter.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(TransactionNotFoundException.class)
+    public ResponseEntity<?> handleTransactionNotFound(TransactionNotFoundException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(Map.of(
+                        "timestamp", LocalDateTime.now(),
+                        "status",404,
+                        "error","Not Found",
+                        "message",ex.getMessage()
+                ));
+    }
+
+}
--- src/main/java/com/example/transactionstarter/service/TransactionServiceImpl.java
+++ src/main/java/com/example/transactionstarter/service/TransactionServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.transactionstarter.service;
 
+import com.example.transactionstarter.exception.TransactionNotFoundException;
 import com.example.transactionstarter.model.Customer;
 import com.example.transactionstarter.model.Transaction;
 import com.example.transactionstarter.model.TransactionStatus;
@@ -36,7 +37,7 @@ public class TransactionServiceImpl  implements TransactionService{
     @Override
     public Transaction getTransaction(String transactionId) {
         return transactionRepository.findById(transactionId)
-                .orElseThrow(() -> new RuntimeException("Transaction not Found" + transactionId));
+                .orElseThrow(() -> new TransactionNotFoundException("Transaction Not Found" + transactionId));
     }
 
     @Override

53e5eff2bd129e87c0341c29f3307004e72954f7
Venkat Aravind <venkataravind.123456@gmail.com>
2026-09-01 15:54:19 +0530

    Four Automated Tests Created

--- src/test/java/com/example/transactionstarter/TransactionServiceImplTests.java
+++ src/test/java/com/example/transactionstarter/TransactionServiceImplTests.java
@@ -1,5 +1,8 @@
 package com.example.transactionstarter;
 
+import com.example.transactionstarter.exception.DuplicateTransactionException;
+import com.example.transactionstarter.exception.TransactionNotFoundException;
+import com.example.transactionstarter.exception.ValidateException;
 import com.example.transactionstarter.model.Customer;
 import com.example.transactionstarter.model.Transaction;
 import com.example.transactionstarter.model.TransactionStatus;
@@ -7,14 +10,14 @@ import com.example.transactionstarter.model.TransactionType;
 import com.example.transactionstarter.repository.CustomerRepository;
 import com.example.transactionstarter.repository.TransactionRepository;
 import com.example.transactionstarter.service.TransactionService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class TransactionServiceImplTests {
@@ -28,6 +31,15 @@ public class TransactionServiceImplTests {
     @Autowired
     private TransactionService transactionService;
 
+    //This method ensures that after each test the database should be wiped off data
+    // So that each test runs in the same database environment.
+    @BeforeEach
+    void setUp(){
+        transactionRepository.deleteAll();
+        customerRepository.deleteAll();
+    }
+    //This Test tests whether the transaction is being created or not.
+    //A Transaction created successfully.
     @Test
     void createTransactionSuccessfully(){
 
@@ -51,6 +63,8 @@ public class TransactionServiceImplTests {
         assertEquals("TX01",result.getTransactionId());
     }
 
+    //This test ensures that a transaction is rejected when invalid data is provided.
+    //A transaction is rejected because it fails validation.
     @Test
     void createTransactionWithInvalidAmount(){
         Customer customer = new Customer();
@@ -60,14 +74,60 @@ public class TransactionServiceImplTests {
         customerRepository.save(customer);
 
         Transaction transaction = new Transaction();
-        transaction.setTransactionId("TX01");
+        transaction.setTransactionId("TX02");
         transaction.setCustomer(customer);
-        transaction.setAmount(BigDecimal.valueOf(100.0));
-        transaction.setCurrency("");
+        transaction.setAmount(BigDecimal.valueOf(0));
+        transaction.setCurrency("INR");
         transaction.setTransactionType(TransactionType.PAYMENT);
         transaction.setTransactionStatus(TransactionStatus.PENDING);
 
-        Transaction result = transactionService.createTransaction(transaction);
+        assertThrows(
+                ValidateException.class,
+                () -> transactionService.createTransaction(transaction)
+        );
+    }
 
+    //This test ensures that a Duplicate Transaction cannot be created.
+    //A duplicate transaction ID rejected.
+    @Test
+    void createDuplicateTransaction(){
+        Customer c = new Customer();
+        c.setCustomerId("C01");
+        c.setName("Aravind");
+        c.setEmail("aravind@gmail.com");
+        customerRepository.save(c);
+
+        Transaction t = new Transaction();
+        t.setTransactionId("TX01");
+        t.setCustomer(c);
+        t.setCurrency("INR");
+        t.setAmount(BigDecimal.valueOf(1000));
+        t.setTransactionType(TransactionType.PAYMENT);
+        t.setTransactionStatus(TransactionStatus.COMPLETED);
+        transactionRepository.save(t);
+
+
+        Transaction t1 = new Transaction();
+        t1.setTransactionId("TX01");
+        t1.setCustomer(c);
+        t1.setCurrency("INR");
+        t1.setAmount(BigDecimal.valueOf(1000));
+        t1.setTransactionType(TransactionType.PAYMENT);
+        t1.setTransactionStatus(TransactionStatus.COMPLETED);
+        assertThrows(
+                DuplicateTransactionException.class,
+                () -> transactionService.createTransaction(t1)
+        );
+
+    }
+
+    //This Test ensures that we cannot retrieve a transaction that does not exist.
+    //A request for transaction that does not exist.
+    @Test
+    void getTransactionThatDoesNotExist(){
+        assertThrows(
+                TransactionNotFoundException.class,
+                () -> transactionService.getTransaction("TX01")
+        );
     }
 }
